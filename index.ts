76d659ef1bfcbe6ed5d128dc631fb1bd0f28c625
Hoa Phan <hoa.phan-van@banvien.com.vn>
2021-04-19 16:24:54 +0700

    clean code

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import CButton from './components/Button';
 import CText from './components/Text';
 import CCheck from './components/Check';
-import { globalLoading } from './components/GlobalLoading';
-import { globalMessage } from './components/GlobalMessage';
+import GlobalLoading, { globalLoading, globalLoadingRef } from './components/GlobalLoading';
+import GlobalMessage, { globalMessage, globalMessageRef } from './components/GlobalMessage';
 import CRecursive from './components/Recursive';
 
-export { CButton, CText, CCheck, CRecursive, globalLoading, globalMessage };
+export { CButton, CText, CCheck, CRecursive, globalLoading, GlobalLoading, globalLoadingRef, globalMessage, GlobalMessage, globalMessageRef };
