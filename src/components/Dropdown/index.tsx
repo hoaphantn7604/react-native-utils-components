63ac7418bba967a9f7fb63cbdb1fb3f31ecc5e0b
Hoa Phan <hoa.phan-van@banvien.com.vn>
2021-05-20 14:02:47 +0700

    update dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Image, ImageStyle, Modal, SafeAreaView, StyleProp, Text, TextStyle, TouchableOpacity, TouchableWithoutFeedback, View, ViewStyle } from 'react-native';
+import { Easing } from 'react-native';
+import { FlatList, Image, ImageStyle, Modal, SafeAreaView, StyleProp, Text, TextStyle, TouchableOpacity, TouchableWithoutFeedback, View, ViewStyle, Animated } from 'react-native';
 import { styles } from './styles';
+import { dimensionsScale } from 'react-native-utils-scale';
+
+const { scale } = dimensionsScale;
+
 
 const ic_check = require('./icon/check.png');
 const ic_down = require('./icon/down.png');
@@ -11,7 +16,7 @@ interface Props {
   iconStyle?: StyleProp<ImageStyle>;
   iconTickStyle?: StyleProp<ImageStyle>;
   headerStyle?: StyleProp<ViewStyle>;
-  labelStyle?: StyleProp<TextStyle>;
+  labelStyle?: StyleProp<ViewStyle>;
   textErrorStyle?: StyleProp<TextStyle>;
   data: any[];
   value?: any | null;
@@ -34,7 +39,9 @@ const defaultProps = {
 const Dropdown: React.FC<Props> = (props) => {
 
   const [visible, setVisible] = useState<boolean>(false);
-  const [currentValue, setCurrentValue] = useState(null);
+  const [currentValue, setCurrentValue] = useState<any>(null);
+  const [height] = useState(new Animated.Value(0));
+
   const {
     onChange,
     data,
@@ -57,15 +64,45 @@ const Dropdown: React.FC<Props> = (props) => {
     getValue();
   }, []);
 
+  const show = () => {
+    Animated.timing(height, {
+      toValue: scale(300),
+      duration: 200,
+      easing: Easing.linear,
+      useNativeDriver: false
+    }).start(() => { });
+  }
+
+  const close = () => {
+    Animated.timing(height, {
+      toValue: 0,
+      duration: 200,
+      easing: Easing.linear,
+      useNativeDriver: false
+    }).start(() => { });
+  }
+
+  useEffect(()=>{
+    if(visible){
+      show();
+    }else{
+      close();
+    }
+  }, [visible])
+
+  const showOrClose = () => {
+    setVisible(!visible);
+  }
+
   const getValue = () => {
     const getItem = data.filter(e => value === e[valueField]);
     if (getItem.length > 0) {
-      setCurrentValue(e => e = getItem[0]);
+      setCurrentValue((e: any) => e = getItem[0]);
     }
   }
 
   const onSelect = (item: any) => {
-    setCurrentValue(e => e = item);
+    setCurrentValue((e: any) => e = item);
     onChange(item[valueField]);
     setVisible(false);
   }
@@ -82,9 +119,7 @@ const Dropdown: React.FC<Props> = (props) => {
 
   const _renderDropdown = () => {
     return (
-      <TouchableWithoutFeedback onPress={() => {
-        setVisible(!visible);
-      }}>
+      <TouchableWithoutFeedback onPress={showOrClose}>
         <View style={styles.dropdown}>
           <Text style={[labelStyle]}>
             {currentValue && currentValue[labelField] || placeholder}
@@ -97,7 +132,7 @@ const Dropdown: React.FC<Props> = (props) => {
 
   const renderItem = ({ item, index }: { item: any; index: number }) => {
     return (
-      <TouchableOpacity onPress={() => { onSelect(item) }}>
+      <TouchableOpacity onPress={() => onSelect(item)}>
         <View style={[styles.item, item[valueField] === (currentValue && currentValue[valueField]) && { backgroundColor: '#F6F7F8' }]}>
           <Text style={[labelStyle]}
           >{item[labelField]}</Text>
@@ -110,37 +145,40 @@ const Dropdown: React.FC<Props> = (props) => {
   };
 
   const _renderList = () => {
-    return <View style={styles.list}>
+    return <Animated.View style={[{ height: height }]}>
       <FlatList
         data={data}
         renderItem={renderItem}
         keyExtractor={(item, index) => index.toString()}
         showsVerticalScrollIndicator={false}
       />
-    </View>
+    </Animated.View>
   }
 
   const _renderModal = () => {
-    return (
-      <Modal visible={visible} animationType={'slide'} transparent>
-        <View style={styles.main}>
-          <TouchableOpacity style={{ flex: 1 }} onPress={() => setVisible(false)}>
-            <View style={{ flex: 1 }} />
-          </TouchableOpacity>
-          <View style={styles.modalContent}>
-            <SafeAreaView>
-              <View style={[styles.header, headerStyle]}>
-                <Text style={styles.headerTitle}>{label}</Text>
-                <TouchableOpacity style={styles.closeIcon} onPress={() => { setVisible(false) }}>
-                  <Image source={ic_close} style={[styles.icon, iconStyle]} />
-                </TouchableOpacity>
-              </View>
-              {_renderList()}
-            </SafeAreaView>
+    if (visible) {
+      return (
+        <Modal visible={visible} animationType={'none'} transparent>
+          <View style={styles.main}>
+            <TouchableOpacity style={{ flex: 1 }} onPress={showOrClose}>
+              <View style={{ flex: 1 }} />
+            </TouchableOpacity>
+            <View style={styles.modalContent}>
+              <SafeAreaView>
+                <View style={[styles.header, headerStyle]}>
+                  <Text style={styles.headerTitle}>{label}</Text>
+                  <TouchableOpacity style={styles.closeIcon} onPress={showOrClose}>
+                    <Image source={ic_close} style={[styles.icon, iconStyle]} />
+                  </TouchableOpacity>
+                </View>
+                {_renderList()}
+              </SafeAreaView>
+            </View>
           </View>
-        </View>
-      </Modal>
-    )
+        </Modal>
+      )
+    }
+    return null
   }
 
   return (
