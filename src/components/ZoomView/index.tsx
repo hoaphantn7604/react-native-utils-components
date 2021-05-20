d87c03d3657ef8ec016e6a96f8a6edc7c973ff70
Hoa Phan <hoa.phan-van@banvien.com.vn>
2021-05-20 17:22:19 +0700

    change zoom view

--- src/components/ZoomView/index.tsx
+++ src/components/ZoomView/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Dimensions, ScrollView, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Dimensions, ScrollView, StyleSheet, View, Button } from 'react-native';
+import { Text } from 'react-native-svg';
 const { width, height } = Dimensions.get('window');
 
 export interface Props {
@@ -7,20 +8,39 @@ export interface Props {
   minimumZoomScale?: number;
   zoomHeight?: number;
   zoomWidth?: number;
+  autoZoom?: boolean;
 }
 
 const defaultProps = {
   maximumZoomScale: 2,
   minimumZoomScale: 1,
+  autoZoom: false,
   zoomHeight: height,
   zoomWidth: width,
 }
 
 const ZoomViewComponent: React.FC<Props> = (props) => {
-  const { maximumZoomScale, minimumZoomScale, zoomHeight, zoomWidth } = props;
+  const { maximumZoomScale, minimumZoomScale, zoomHeight, zoomWidth, autoZoom } = props;
+  const scrollRef = useRef<any>(null);
+
+  const onDefault = () => {
+    if (scrollRef.current) {
+      scrollRef.current.getScrollResponder().scrollResponderZoomTo({animated: false});
+    }
+  }
+
+  useEffect(() => { 
+    if(autoZoom){
+      setTimeout(() => {
+        onDefault();
+      }, 50);
+    }
+  }, []);
+
   return (
       <View style={{ width: zoomWidth, height: zoomHeight }}>
         <ScrollView
+          ref={scrollRef}
           maximumZoomScale={maximumZoomScale}
           minimumZoomScale={minimumZoomScale}
           showsHorizontalScrollIndicator={false}
